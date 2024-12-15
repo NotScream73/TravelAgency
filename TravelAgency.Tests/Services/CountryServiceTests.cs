e93d1b27eeec7bac274326bda9e8eb832dfc9fec
NotScream73 <112487179+NotScream73@users.noreply.github.com>
2024-12-15 20:32:01 +0400

    Add tests (#8)
    * #20 Implement CRUD for country (#1)
    
    * Add very simple crud for country. Add Identity template refs #20
    
    * revert identity refs #20
    
    * add tests refs #20
    
    * add crud for resort refs #22 (#2)
    
    * Refactore country, add service layout refs #20 (#3)
    
    * Add CRUD Accommodation refs #21 (#5)
    
    * refs #15 (#6)
    
    * refs #20
    
    * refs #20
    
    * tests

--- TravelAgency.Tests/Services/CountryServiceTests.cs
+++ TravelAgency.Tests/Services/CountryServiceTests.cs
@@ -1,173 +1,123 @@
-﻿/*using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Moq;
 using TravelAgency.Data;
 using TravelAgency.Models;
+using TravelAgency.Models.DTO;
 using TravelAgency.Services;
 
-namespace TravelAgency.Tests.Services
+namespace TravelAgency.Tests.Services;
+
+[TestClass]
+public class CountryServiceTests
 {
-    [TestClass]
-    public class CountryServiceTests
+    private DataContext CreateInMemoryContext()
     {
-        [TestMethod]
-        public async Task GetAllCountriesAsync_ShouldReturnListOfCountries()
-        {
-            var options = new DbContextOptionsBuilder<DataContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
 
-            var _context = new DataContext(options);
-            var _service = new CountryService(_context);
-
-            _context.Countries.AddRange(new List<Country>
-            {
-                new Country { Id = 1, Name = "Россия" },
-                new Country { Id = 2, Name = "Китай" }
-            });
-
-            await _context.SaveChangesAsync();
-
-            var service = new CountryService(_context);
-
-            var result = await service.GetAllCountriesAsync();
-
-            Assert.IsNotNull(result);
-            Assert.AreEqual(2, result.Count);
-            Assert.AreEqual("Россия", result.First().Name);
-        }
-
-        [TestMethod]
-        public async Task GetCountryByIdAsync_ShouldReturnCountry_WhenIdExists()
-        {
-            var options = new DbContextOptionsBuilder<DataContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
-
-            var _context = new DataContext(options);
-            var _service = new CountryService(_context);
-
-            _context.Countries.AddRange(new List<Country>
-            {
-                new Country { Id = 1, Name = "Россия" },
-                new Country { Id = 2, Name = "Китай" }
-            });
-            await _context.SaveChangesAsync();
-
-            var service = new CountryService(_context);
-
-            var result = await service.GetCountryByIdAsync(1);
-
-            Assert.IsNotNull(result);
-            Assert.AreEqual("Россия", result.Name);
-        }
-
-        [TestMethod]
-        public async Task GetCountryByIdAsync_ShouldReturnNull_WhenIdDoesNotExist()
-        {
-            var options = new DbContextOptionsBuilder<DataContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
-
-            var _context = new DataContext(options);
-
-            _context.Countries.AddRange(new List<Country>
-            {
-                new Country { Id = 1, Name = "Россия" }
-            });
-            await _context.SaveChangesAsync();
-
-            var service = new CountryService(_context);
-
-            var result = await service.GetCountryByIdAsync(99);
-
-            Assert.IsNull(result);
-        }
-
-        [TestMethod]
-        public async Task CreateCountryAsync_ShouldReturnTrue_WhenCountryIsCreated()
-        {
-            var options = new DbContextOptionsBuilder<DataContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
-
-            var _context = new DataContext(options);
-            var _service = new CountryService(_context);
-
-            var country = new Country
-            {
-                Id = 1,
-                Name = "Россия"
-            };
-
-            var result = await _service.CreateCountryAsync(country);
-
-            Assert.IsTrue(result);
-        }
-
-        [TestMethod]
-        public async Task UpdateCountryAsync_ShouldReturnTrue_WhenCountryIsUpdated()
-        {
-            var options = new DbContextOptionsBuilder<DataContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
-
-            var _context = new DataContext(options);
-            var _service = new CountryService(_context);
-
-            var country = new Country
-            {
-                Id = 1,
-                Name = "Россия"
-            };
-
-            _context.Add(country);
-
-            await _context.SaveChangesAsync();
-
-            country.Name = "Россия топ";
-            var result = await _service.UpdateCountryAsync(country);
-
-            Assert.IsTrue(result);
-        }
-
-        [TestMethod]
-        public async Task DeleteCountryAsync_ShouldReturnTrue_WhenCountryIsDeleted()
-        {
-            var options = new DbContextOptionsBuilder<DataContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
-
-            var _context = new DataContext(options);
-            var _service = new CountryService(_context);
-
-            var country = new Country
-            {
-                Id = 1,
-                Name = "Украина"
-            };
-
-            _context.Add(country);
-
-            await _context.SaveChangesAsync();
-
-            var result = await _service.DeleteCountryAsync(1);
-
-            Assert.IsTrue(result);
-        }
-
-        [TestMethod]
-        public async Task DeleteCountryAsync_ShouldReturnFalse_WhenCountryDoesNotExist()
-        {
-            var options = new DbContextOptionsBuilder<DataContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
-
-            var _context = new DataContext(options);
-            var _service = new CountryService(_context);
-
-            var countryId = 99;
-            var result = await _service.DeleteCountryAsync(countryId);
-
-            Assert.IsFalse(result);
-        }
+        return new DataContext(options);
     }
-}*/
+
+    [TestMethod]
+    public async Task GetAllForSelectAsync_ReturnsAllCountriesInOrder()
+    {
+        var context = CreateInMemoryContext();
+        context.Countries.AddRange(
+            new Country { Id = 1, Name = "Россия" },
+            new Country { Id = 2, Name = "Китай" }
+        );
+        await context.SaveChangesAsync();
+
+        var mapperMock = new Mock<IMapper>();
+        var service = new CountryService(context, mapperMock.Object);
+
+        var result = await service.GetAllForSelectAsync();
+
+        Assert.AreEqual(2, result.Count);
+        Assert.AreEqual("Китай", result[0].Name);
+        Assert.AreEqual("Россия", result[1].Name);
+    }
+
+    [TestMethod]
+    public async Task GetByIdAsync_ReturnsCountryDetails_WhenCountryExists()
+    {
+        var context = CreateInMemoryContext();
+        context.Countries.Add(new Country { Id = 1, Name = "Россия" });
+        await context.SaveChangesAsync();
+
+        var mapperMock = new Mock<IMapper>();
+        mapperMock.Setup(m => m.ConfigurationProvider).Returns(new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Country, CountryDetailsDTO>();
+        }));
+
+        var service = new CountryService(context, mapperMock.Object);
+
+        var result = await service.GetByIdAsync(1);
+
+        Assert.IsNotNull(result);
+        Assert.AreEqual(1, result.Id);
+        Assert.AreEqual("Россия", result.Name);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(Exception), "Страна не найдена")]
+    public async Task GetByIdAsync_ThrowsException_WhenCountryDoesNotExist()
+    {
+        var context = CreateInMemoryContext();
+        var mapperMock = new Mock<IMapper>();
+        mapperMock.Setup(m => m.ConfigurationProvider).Returns(new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Country, CountryDetailsDTO>();
+        }));
+
+        var service = new CountryService(context, mapperMock.Object);
+
+        await service.GetByIdAsync(999);
+    }
+
+    [TestMethod]
+    public async Task CreateAsync_AddsCountryToDatabase()
+    {
+        var context = CreateInMemoryContext();
+        var mapperMock = new Mock<IMapper>();
+        mapperMock.Setup(m => m.Map<Country>(It.IsAny<CountryCreateDTO>()))
+            .Returns((CountryCreateDTO dto) => new Country { Name = dto.Name });
+
+        var service = new CountryService(context, mapperMock.Object);
+        var countryDto = new CountryCreateDTO { Name = "Россия" };
+
+        await service.CreateAsync(countryDto);
+
+        Assert.AreEqual(1, context.Countries.Count());
+        Assert.AreEqual("Россия", context.Countries.First().Name);
+    }
+
+    [TestMethod]
+    public async Task DeleteAsync_RemovesCountryFromDatabase()
+    {
+        var context = CreateInMemoryContext();
+        context.Countries.Add(new Country { Id = 1, Name = "Украина" });
+        await context.SaveChangesAsync();
+
+        var mapperMock = new Mock<IMapper>();
+        var service = new CountryService(context, mapperMock.Object);
+
+        await service.DeleteAsync(1);
+
+        Assert.AreEqual(0, context.Countries.Count());
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(Exception), "Страна не найдена")]
+    public async Task DeleteAsync_ThrowsException_WhenCountryDoesNotExist()
+    {
+        var context = CreateInMemoryContext();
+        var mapperMock = new Mock<IMapper>();
+        var service = new CountryService(context, mapperMock.Object);
+
+        await service.DeleteAsync(999);
+    }
+}
